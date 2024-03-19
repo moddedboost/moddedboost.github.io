97fdf0cb429a3e30585cdc611289b7193a08e491
descatal <exvsfbmod@gmail.com>
2024-03-19 15:52:47 +0800

    Updating docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'moddedboost', // Usually your GitHub org/user name.
+  projectName: 'moddedboost-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
